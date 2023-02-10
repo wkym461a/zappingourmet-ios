3fe1bbcb41e5da54739424b83e63254030aa01e4
wkym461a <58236917+wkym461a@users.noreply.github.com>
2023-02-10 17:50:23 +0900

    [Add] Moya and HotPepperAPI sample

--- Zappingourmet/ViewController.swift
+++ Zappingourmet/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Moya
 
 class ViewController: UIViewController {
     
@@ -15,12 +16,38 @@ class ViewController: UIViewController {
     
     // MARK: - Property
     
+    private var labelText: String = "Hello, Zappingourmet!"
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupUI()
+        self.updateUI()
+        
+        let provider = MoyaProvider<HotPepperAPI>()
+        let homeLatitude: Double = 35.17454481366307
+        let homeLongitude: Double = 136.91228418325178
+        provider.request(.gourmetSearch(latitude: homeLatitude, longitude: homeLongitude, range: .oneThousand)) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .success(response):
+                do {
+                    let json = try response.mapJSON()
+                    print(json)
+                    self.labelText = "Success HotPepperAPI!"
+                    self.updateUI()
+                    
+                } catch {
+                    print(error)
+                }
+                
+            case let .failure(moyaError):
+                print(moyaError)
+            }
+        }
     }
     
     // MARK: - Public
@@ -28,11 +55,11 @@ class ViewController: UIViewController {
     // MARK: - Private
     
     private func setupUI() {
-        self.label.text = "Hello, Zappingourmet!"
         self.label.textColor = .orange
     }
     
     private func updateUI() {
+        self.label.text = self.labelText
     }
     
     // MARK: - Action
